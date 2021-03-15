2b812b9343ef4bc21086581465692e98a6748b8e
Claudiu Popa <claudiupopa330@gmail.com>
2021-03-15 19:07:16 +0200

    refactor(base): Add application base usage

--- core/Application.php
+++ core/Application.php
@@ -3,5 +3,44 @@
 namespace App\Core;
 
 class Application{
+
+    public static string $ROOT_DIR;
+    public Router $router;
+    public Request $request;
+    public Response $response;
+    public static Application $app;
+    protected static $registry = [];
+
+    /**
+     * Application constructor.
+     * @param $router
+     */
+    public function __construct($rootPath)
+    {
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
+
+    }
+
+    public static function bind($key, $value)
+    {
+        static::$registry[$key] = $value;
+    }
+
+    public static function get($key)
+    {
+        if(!array_key_exists($key, static::$registry)) {
+            throw new \Exception("No {$key} is bound in the container.");
+        }
+
+        return static::$registry[$key];
+    }
+
+    public function run()
+    {
+        echo $this->router->resolve();
     }
 }
