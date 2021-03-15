1e684a4d665596b7eb92ece43555d981f129bce9
Claudiu Popa <claudiupopa330@gmail.com>
2021-03-15 21:33:52 +0200

    refactor(base): Add layout prop

--- core/Controller.php
+++ core/Controller.php
@@ -6,7 +6,23 @@ namespace App\Core;
 
 class Controller
 {
-    public function render($view, $params = []){
+    protected string $layout = 'main';
+
+    public function render($view, $params = [])
+    {
         return Application::$app->router->renderView($view, $params);
+    }
+
+    public function setLayout($layoutName)
+    {
+        $this->layout = $layoutName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLayout(): string
+    {
+        return $this->layout;
     }
 }
