287cff9fb4773bea9fd799a21bf13df59fc0f613
Claudiu Popa <claudiupopa330@gmail.com>
2021-03-15 19:08:28 +0200

    feat(base): Create Request class

---
+++ core/Request.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Core;
+
+
+class Request
+{
+    public function getPath()
+    {
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
+        $questionMarkPosition = strpos($path, '?');
+
+        if($questionMarkPosition === false){
+            return $path;
+        }
+        $path = substr($path, 0, $questionMarkPosition);
+
+        return $path;
+    }
+
+    public function getMethod()
+    {
+        return strtolower($_SERVER["REQUEST_METHOD"]);
+    }
+}
