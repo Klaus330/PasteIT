b8b4e4c9a50d74daab2527a13ad0ea8756d29a6b
Claudiu Popa <claudiupopa330@gmail.com>
2021-03-16 19:40:48 +0200

    refactor(base): Add rules constants

--- core/Validator.php
+++ core/Validator.php
@@ -5,4 +5,10 @@ namespace app\core;
 
 class Validator
 {
+    public const RULE_REQUIRED = 'required';
+    public const RULE_EMAIL = 'email';
+    public const RULE_UNIQUE = 'unique';
+    public const RULE_MATCH = 'match';
+    public const RULE_MIN = 'min';
+    public const RULE_MAX = 'min';
 }
