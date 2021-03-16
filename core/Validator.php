6b5943e549b4e1257a413e8a5afb9a28fd991fd9
Claudiu Popa <claudiupopa330@gmail.com>
2021-03-16 19:41:40 +0200

    refactor(base): Add validate method

--- core/Validator.php
+++ core/Validator.php
@@ -11,4 +11,51 @@ class Validator
     public const RULE_MATCH = 'match';
     public const RULE_MIN = 'min';
     public const RULE_MAX = 'min';
+    public static function validate($data, $rulesArray)
+    {
+        if($rulesArray === [] || $data === [])
+        {
+            var_dump($rulesArray);
+            throw new \InvalidArgumentException('No Rules Provided.');
+        }
+
+        foreach ($rulesArray as $attribute => $rules) {
+            $value = $data[$attribute] ?? null;
+            foreach ($rules as $rule) {
+                $ruleName = $rule;
+                if (is_array($ruleName)) {
+                    $ruleName = $rule[0];
+                }
+
+                switch ($ruleName) {
+                    case Validator::RULE_REQUIRED:
+                        if ($value != null) {
+                            self::addError($attribute, self::RULE_REQUIRED);
+                        }
+                        break;
+                    case Validator::RULE_EMAIL:
+                        if(!filter_var($value, FILTER_VALIDATE_EMAIL)){
+                            self::addError($attribute, self::RULE_EMAIL);
+                        }
+                        break;
+                    case Validator::RULE_MIN:
+                        if(strlen($value) < $rule['min']){
+                            self::addError($attribute, self::RULE_MIN, $rule);
+                        }
+                        break;
+                    case Validator::RULE_MAX:
+                        if(strlen($value) < $rule['max']){
+                            self::addError($attribute, self::RULE_MAX, $rule);
+                        }
+                        break;
+                    case Validator::RULE_MATCH:
+                            if($value !== $data[$rule['match']]){
+                                self::addError($attribute, self::RULE_MATCH, $rule);
+                            }
+                        break;
+                }
+            }
+        }
+        return self::$errors;
+    }
 }
