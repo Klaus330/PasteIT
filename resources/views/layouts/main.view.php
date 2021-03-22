921b5ecbec04d99e59f4d01be8a2e5515a20b2f7
Claudiu Popa <claudiupopa330@gmail.com>
2021-03-22 15:40:57 +0200

    refactor(MOD-3): Add simple navbar

--- resources/views/layouts/main.view.php
+++ resources/views/layouts/main.view.php
@@ -6,12 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="/css/style.css">
+
 </head>
 <body>
+<?php require dirname(__DIR__)."/partials/nav.view.php" ?>
 
 <div class="container">
     {{content}}
 </div>
 
+
+<script src="/js/script.js"></script>
 </body>
 </html>
