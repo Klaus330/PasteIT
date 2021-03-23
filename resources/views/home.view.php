d44cbc728b28b43024a79b6cb17e8c4acb1ec518
Rata Cristina Iulia <cristinaiulia.rata@gmail.com>
2021-03-23 18:32:46 +0200

    refactor (MOD-2): change home form style

--- resources/views/home.view.php
+++ resources/views/home.view.php
@@ -29,28 +29,28 @@
     <div class="row">
         <h4>Optional Paste Settings</h4>
         <div>
-            <form action="">
+            <form class="home-form" action="">
                 <div class="form-group">
-                    <label for="">Syntax Highlighting:</label>
-                    <select name="" id="" class="form-control">
+                    <label class="form-label" for="">Syntax Highlighting:</label>
+                    <select name="" id="" class="form-select">
                         <option value="">None</option>
                         <option value="">C++</option>
                     </select>
                 </div>
                 <div class="form-group">
-                    <label for="">Paste Exposure:</label>
-                    <select name="" id="" class="form-control">
+                    <label class="form-label" for="">Paste Exposure:</label>
+                    <select name="" id="" class="form-select">
                         <option value="">None</option>
                         <option value="">Public</option>
                         <option value="">Private</option>
                     </select>
                 </div>
-                <div class="form-group">
-                    <label for=""></label>
-                    <input type="checkbox" placeholder="Burn after read" class="form-control">
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input">
+                    <label class="form-label" for="">Burn after read</label>
                 </div>
                 <div class="form-group">
-                    <label for="">Paste Name/Title:</label>
+                    <label class="form-label" for="">Paste Name/Title:</label>
                     <input type="text" placeholder="Title" class="form-control">
                 </div>
                 <div class="form-group">
