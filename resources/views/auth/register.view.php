4eeb28f6240ae8355cb0b3353a2af294e49647ab
SanduVAdelin <adelinsandu26@gmail.com>
2021-03-24 20:35:20 +0200

    refactor (MOD-1) : make register view responsive

--- resources/views/auth/register.view.php
+++ resources/views/auth/register.view.php
@@ -3,45 +3,45 @@
     <div class="row">
         <form class="register-form" action="/register" method="POST">
             <div class="grid" >
-                <div class="col-3">
+                <div class="col-md-3 col-3">
                 <label for="username" class="form-label">
                     Username:
                 </label>
                 </div>
-                <div class="col-9 flex align-center">
+                <div class="col-md-9 col-12 flex align-center">
                 <input type="text" name="username" placeholder="username" class="form-control"/>
                 </div>
             </div>
 
             <div class="grid" >
-                <div class="col-3">
+                <div class="col-md-3 col-3">
                 <label for="email" class="form-label">
                     Email:
                 </label>
                 </div>
-                <div class="col-9 flex align-center">
+                <div class="col-md-9 col-12 flex align-center">
                 <input type="email" name="email" placeholder="email" class="form-control"/>
                 </div>
                 
             </div>
             
             <div class="grid">
-                <div class="col-3">
+                <div class="col-md-3 col-3">
                     <label for="password" class="form-label">
                     Password:
                 </label></div>
-                <div class="col-9 flex align-center">
+                <div class="col-md-9 col-12 flex align-center">
                 <input type="password" name="password" placeholder="password" class="form-control"/>
                 </div>
         
             </div>
             <div class="grid">
-                <div class="col-3">
+                <div class="col-md-3 col-3">
                 <label for="confirm-password" class="form-label line-height-sm">
                     Confirm password:
                 </label>
                 </div>
-                <div class="col-9 flex align-center">
+                <div class="col-md-9 col-12 flex align-center">
                 <input type="password" name="confirm-password" placeholder="confirm password" class="form-control"/>
                 </div>
                 
