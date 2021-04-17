380ee881a577c1d179ab4d2f0d1c1514f1cf1971
Claudiu Popa <claudiupopa330@gmail.com>
2021-04-17 10:59:19 +0300

    refactor(base): Add the posibility to reset migrations

--- migrations.php
+++ migrations.php
@@ -1,9 +1,20 @@
 <?php
+
 use app\core\Application;
 use app\migrations\Migration;
-require_once __DIR__."/vendor/autoload.php";
+
+require_once __DIR__ . "/vendor/autoload.php";
 $config = require_once "config.php";
 
 $app = new Application(__DIR__, $config);
 
-$app->db->applyMigrations();
+
+if ($argv[1] == "-up") {
+    $app->db->applyMigrations();
+}else if($argv[1] == "-down"){
+    $app->db->downMigrations();
+}else{
+    echo 'I do not know this command. The available commands are:'.PHP_EOL;
+    echo 'php migrations.php -up - Creates all the tables specified in migrations'.PHP_EOL;
+    echo 'php migrations.php -down - Deletes all the migrations and tables from the db.';
+}
