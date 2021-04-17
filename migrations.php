437d75e267f96e9921c3d062f2becbc63c97f46b
Claudiu Popa <claudiupopa330@gmail.com>
2021-04-17 11:03:36 +0300

    fix(base): Add posibility to reset migrations

--- migrations.php
+++ migrations.php
@@ -9,12 +9,16 @@ $config = require_once "config.php";
 $app = new Application(__DIR__, $config);
 
 
-if ($argv[1] == "-up") {
+if ($argv[1] == "-migrate") {
     $app->db->applyMigrations();
-}else if($argv[1] == "-down"){
+} else if ($argv[1] == "-down") {
     $app->db->downMigrations();
-}else{
-    echo 'I do not know this command. The available commands are:'.PHP_EOL;
-    echo 'php migrations.php -up - Creates all the tables specified in migrations'.PHP_EOL;
+} else if ($argv[1] == '-reset') {
+    $app->db->downMigrations();
+    $app->db->applyMigrations();
+} else {
+    echo 'I do not know this command. The available commands are:' . PHP_EOL;
+    echo 'php migrations.php -migrate - Creates all the tables specified in migrations' . PHP_EOL;
     echo 'php migrations.php -down - Deletes all the migrations and tables from the db.';
+    echo 'php migrations.php -reset - Resets the migrations.';
 }
