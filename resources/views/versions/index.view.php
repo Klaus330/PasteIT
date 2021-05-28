68840c6948a182089a6b330c2eda6ff385191828
SanduVAdelin <adelinsandu26@gmail.com>
2021-05-28 16:37:49 +0300

    refactor(MOD-70) : style table versions
    Co-Authored-By: RataCristinaIulia <75426462+RataCristinaIulia@users.noreply.github.com>
    Co-Authored-By: Claudiu Popa <claudiup340@gmail.com>

--- resources/views/versions/index.view.php
+++ resources/views/versions/index.view.php
@@ -1,14 +1,17 @@
 <section class="paste-section">
-    <h2>Versions for <a href="/paste/view/<?= $paste->slug ?>"><?= $paste->title ?></a></h2>
+    <h2 class="text-color">Versions for <a href="/paste/view/<?= $paste->slug ?>"><?= $paste->title ?></a></h2>
 
-    <div class="home-first">
-        <table>
+    <div class="version-table">
+        <table class="table">
+            <thead>
             <tr>
-                <th>Editor</th>
-                <th>Created At</th>
-                <th>Updated At</th>
-                <th>Actions</th>
+                <th scope="col">Editor</th>
+                <th scope="col">Created At</th>
+                <th scope="col">Updated At</th>
+                <th scope="col">Actions</th>
             </tr>
+            </thead>
+
             <tbody>
             <?php foreach ($versions as $version): ?>
                 <tr>
@@ -18,7 +21,7 @@
                     <td><?= date("Y-m-d H:i:s", strtotime($version->updated_at)); ?></td>
                     <td class="flex">
                         <a href="/paste/version/<?= $version->slug ?>" class="btn btn-primary mr-1">View</a>
-                        <form action="/versions/delete/<?=$version->slug?>" method="POST">
+                        <form action="/versions/delete/<?= $version->slug ?>" method="POST">
                             <button class="btn-danger btn">Delete</button>
                         </form>
                     </td>
@@ -26,5 +29,6 @@
             <?php endforeach; ?>
             </tbody>
         </table>
+
     </div>
 </section>
