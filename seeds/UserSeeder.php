bdc2a0c91008bea442e01c7cc78634b660be366f
Claudiu Popa <claudiupopa330@gmail.com>
2021-06-07 21:11:14 +0300

    feat(dev): add 2 default users in the seeder
    Co-Authored-By: SanduVAdelin <58731694+SanduVAdelin@users.noreply.github.com>
    Co-Authored-By: RataCristinaIulia <75426462+RataCristinaIulia@users.noreply.github.com>

--- seeds/UserSeeder.php
+++ seeds/UserSeeder.php
@@ -10,9 +10,29 @@ class UserSeeder implements \app\core\Seeder
     public function run()
     {
         $user = new User();
-        $user->username="guest";
-        $user->email="admin@admin.com";
+        $user->username="admin";
+        $user->email="admin@paste.it";
         $user->password="admin";
+        $user->avatar = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png';
+        $user->save();
+
+        $settings = Settings::create(
+            [
+                'id_user' => User::findOne(['email' => $user->email])->id,
+                'id_syntax' => 1,
+                'exposure' => '',
+                'expiration' => ''
+            ]
+        );
+
+        $settings->save();
+
+
+
+        $user = new User();
+        $user->username="guest";
+        $user->email="guest@paste.it";
+        $user->password="guest";
         $user->avatar = 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png';
         $user->save();
 
